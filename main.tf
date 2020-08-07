32aa8e66934c7b0ab3e5a81acdcdb5951abc69af
shailyn <sortizjim@gmail.com>
2020-08-06 20:48:07 -0400

    Adding terraform deployment specification

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider aws {
+    region = "us-east-1"
+}
+
+resource "aws_ecs_cluster" "super_cool_cluster" {
+    name = "super-cool-cluster"
+    setting {
+        name = "containerInsights"
+        value = "enabled"
+    }
+    capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+}
+
+resource "aws_ecs_task_definition" "api_task" {
+    container_definitions   = file("task_definitions/service.json")
+    cpu                     = 256
+    family                  = "backend_api"
+    memory                  = 512
+    network_mode            = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+}
+
+resource "aws_ecs_service" "api_service" {
+    name                = "backend_api"
+    cluster             = aws_ecs_cluster.super_cool_cluster.arn
+    task_definition     = aws_ecs_task_definition.api_task.arn
+    desired_count       = 1
+    network_configuration {
+      assign_public_ip  = true
+      security_groups   = ["sg-035ab2b542979b957"]
+      subnets = [
+            "subnet-052f463b",
+            "subnet-bf613bd8"
+        ]
+    }
+    launch_type = "FARGATE"
+}
